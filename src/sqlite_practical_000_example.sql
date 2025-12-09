700d9d92eb09b33c1e763071b3b61a370005a57a
py10181 <philip.yaroshchyk@gmail.com>
2025-12-09 13:58:42 +1100

    Initial commit

---
+++ src/sqlite_practical_000_example.sql
@@ -0,0 +1,7 @@
+-- database: ../runtime/db/starwars.db
+
+-- The first line (above) must be present in all SQL script (.sql) files.
+-- It is used by the VSCode SQLite extension(s) to locate the database
+-- file. SQL statements follow. For example:
+
+SELECT * FROM characters;
